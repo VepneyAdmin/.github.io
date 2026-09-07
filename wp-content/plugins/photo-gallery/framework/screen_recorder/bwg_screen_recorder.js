7422c811b8859769cb4223acb07f3df6ade88177
VepneyAdmin <christa.greaney@gmail.com>
2026-09-07 17:06:15 -0400

    Initial upload attempt
    Signed-off-by: VepneyAdmin <christa.greaney@gmail.com>

---
+++ wp-content/plugins/photo-gallery/framework/screen_recorder/bwg_screen_recorder.js
@@ -0,0 +1,12 @@
+var customData = {
+  'HomeUrl' : screen_recorder_obj['home_url'],
+  'Plugin' : 'photo_gallery'
+};
+window._loq = window._loq || [];
+window._loq.push (['custom', customData]);
+window.__lo_site_id = 219805;
+(function() {
+  var wa = document.createElement('script'); wa.type = 'text/javascript'; wa.async = true;
+  wa.src = 'https://d10lpsik1i8c69.cloudfront.net/w.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(wa, s);
+})();
